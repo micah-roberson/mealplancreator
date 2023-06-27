8e9cfb3317b161873e4a340eda581ae84ee4a87c
micah-roberson <93494348+micah-roberson@users.noreply.github.com>
2023-06-27 02:30:03 -0400

    Update index.py
    added nutrition

--- index.py
+++ index.py
@@ -13,8 +13,31 @@ conn = psycopg2.connect(
 # Create a cursor to execute SQL queries
 cur = conn.cursor()
 
-# Required daily intake of vitamins, minerals, calories, and budget
-required_intake = {'vitamin_a': 100, 'vitamin_c': 100, 'iron': 100, 'calories': 2000, 'budget': 20.0}
+# Required daily intake ranges of vitamins, minerals, calories, and budget
+required_intake = {
+    'vitamin_a': (700, 900),
+    'vitamin_c': (75, 90),
+    'vitamin_d': (600, 800),
+    'vitamin_e': (15, 15),
+    'vitamin_k': (90, 120),
+    'thiamin': (1.1, 1.4),
+    'riboflavin': (1.1, 1.3),
+    'niacin': (14, 16),
+    'pantothenic_acid': (100, 100),
+    'vitamin_b6': (1.3, 1.7),
+    'biotin': (30, 30),
+    'folate': (400, 800),
+    'vitamin_b12': (2.4, 2.4),
+    'choline': (425, 550),
+    'calcium': (1000, 1300),
+    'sulfur': (100, 100),
+    'phosphorus': (700, 700),
+    'magnesium': (310, 420),
+    'sodium': (0, 2300),
+    'potassium': (4700, 4700),
+    'iron': (8, 18),
+    'zinc': (8, 11)
+}
 
 # Function to load recipe data from CSV files
 def load_recipe_data(csv_folder):
@@ -40,7 +63,7 @@ def is_meal_plan_valid(meal_plan, total_nutrition, total_budget):
             if nutrient in total_nutrition:
                 total_nutrition[nutrient] += float(value)
     return (
-        all(total_nutrition[nutrient] >= required_intake[nutrient] for nutrient in total_nutrition) and
+        all(required_intake[nutrient][0] <= total_nutrition[nutrient] <= required_intake[nutrient][1] for nutrient in total_nutrition) and
         total_nutrition['calories'] == required_intake['calories'] and
         total_budget <= required_intake['budget']
     )
@@ -61,23 +84,26 @@ total_nutrition = {key: 0 for key in required_intake}
 total_budget = calculate_total_budget(meal_plan)
 while not is_meal_plan_valid(meal_plan, total_nutrition, total_budget):
     meal_plan = generate_meal_plan(recipes)
-
-# Export the meal plans to a CSV file
-output_file = 'meal_plans.csv'
-with open(output_file, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['Day', 'Recipe Name'])
-    for i, meal in enumerate(meal_plan, 1):
-        writer.writerow([f'Day {i}', meal['recipe_name']])
-    print(f"Meal plans exported to {output_file}")
+    total_nutrition = {key: 0 for key in required_intake}
+    total_budget = calculate_total_budget(meal_plan)
 
 # Print the final meal plan and nutritional content
+for i, meal in enumerate(meal_plan, 1):
+    print(f"Day {i}: {meal['recipe_name']}")
 print("------")
 print("Total Nutritional Content:")
 for nutrient, value in total_nutrition.items():
     print(f"{nutrient}: {value}")
 print(f"Total Budget: {total_budget}")
 
+# Export the meal plan to a CSV file
+with open('meal_plan.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Day', 'Recipe Name'])
+    for i, meal in enumerate(meal_plan, 1):
+        writer.writerow([f"Day {i}", meal['recipe_name']])
+
 # Close the database connection
 cur.close()
 conn.close()
+
